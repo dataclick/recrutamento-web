aad3ce0cd8fa3ec3f500f907d3eb60d37c148548
Ricardo Pires <ricardo.tech@live.com>
2017-05-09 20:36:36 -0300

    Mudando operador ternario para ser compativel com a versao 5.6 do PHP

--- aplicativo/resources/views/clubs/form.blade.php
+++ aplicativo/resources/views/clubs/form.blade.php
@@ -1,7 +1,7 @@
 {{ csrf_field() }}
 <div class="form-group">
     <label for="name">Nome:</label>
-    <input type="text" class="form-control" name="name" id="name" placeholder="Nome do clube..." value="{{ $club->name ?? '' }}" maxlength="100" required>
+    <input type="text" class="form-control" name="name" id="name" placeholder="Nome do clube..." value="{{ isset($club->name) ? $club->name : '' }}" maxlength="100" required>
     @if ($errors->has('name'))
         <p class="help-block">
             <strong class="text-danger">{{ $errors->first('name') }}</strong>
--- aplicativo/resources/views/partners/form.blade.php
+++ aplicativo/resources/views/partners/form.blade.php
@@ -1,7 +1,7 @@
 {{ csrf_field() }}
 <div class="form-group">
     <label for="name">Nome:</label>
-    <input type="text" class="form-control" name="name" id="name" placeholder="Nome completo..." value="{{ $partner->name ?? '' }}" maxlength="100" required>
+    <input type="text" class="form-control" name="name" id="name" placeholder="Nome completo..." value="{{ isset($partner->name) ? $partner->name : '' }}" maxlength="100" required>
     @if ($errors->has('name'))
         <p class="help-block">
             <strong class="text-danger">{{ $errors->first('name') }}</strong>
@@ -11,15 +11,19 @@
 
 @if(count($clubs) > 0)
     <label>Clubes:</label>
-    <div class="well">
+    <div class="row">
         @foreach($clubs as $club)
-            <label class="checkbox-inline">
-                <input
-                    type="checkbox"
-                    name="clubs[]"
-                    value="{{ $club->id }}"
-                    {{ isset($associates) && in_array($club->id, $associates) ? 'checked' : '' }}> {{ $club->name }}
-            </label>
+            <div class="col-xs-4 col-sm-3">
+                <div class="checkbox">
+                    <label>
+                        <input
+                            type="checkbox"
+                            name="clubs[]"
+                            value="{{ $club->id }}"
+                            {{ isset($associates) && in_array($club->id, $associates) ? 'checked' : '' }}> {{ $club->name }}
+                    </label>
+                </div>
+            </div>
         @endforeach
     </div>
 @endif
